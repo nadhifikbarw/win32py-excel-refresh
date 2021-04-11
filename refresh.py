d71e9aa10258537615a88b2a83250fee8b34e81f
Nadhif Ikbar Wibowo <10264286+nadhifikbarw@users.noreply.github.com>
2021-04-11 18:32:19 +0700

    Add headless parameter and refactor in English

--- refresh.py
+++ refresh.py
@@ -1,38 +1,42 @@
 from os.path import abspath
 import win32com.client as win32
-# So Far library ini hanya ada di Windows, kemungkinan tidak tersedia untuk Mac/Linux
+# Only available for Windows
 
-
-def open_excel():
+def open_excel(headless=True):
   excel = win32.gencache.EnsureDispatch('Excel.Application')
-  excel.Visible = False
+  excel.Visible = !headless # Default to run in headless-mode
   return excel
 
 def open_workbook(excel, path):
   return excel.Workbooks.Open(Filename=path)
 
-
 # References: https://stackoverflow.com/questions/40893870/refresh-excel-external-data-with-python
 # VBA References: https://docs.microsoft.com/en-us/office/vba/api/overview/excel/object-model
 
 def main():
-  # Buka Excel
-  path = abspath('dashboard.xlsx')
+  # Open Excel.Application, make sure you close all running Excel instances before running this script 
   excel = open_excel()
+  
+  # Open the dashboard Excel file
+  path = abspath('dashboard.xlsx')
   wb = open_workbook(excel, path)
 
-  # Melakukan query ulang dengan menjalankan Refresh All pada seluruh 
+  # Trigger 'Refresh All'
   wb.RefreshAll()
-  # Menunggu hingga semua Async Queries selesai. Refer to: https://docs.microsoft.com/en-us/office/vba/api/excel.application.calculateuntilasyncqueriesdone 
+  
+  # Wait until all async queries done
+  # Refer to: https://docs.microsoft.com/en-us/office/vba/api/excel.application.calculateuntilasyncqueriesdone 
   excel.CalculateUntilAsyncQueriesDone() 
+  
   # Close dan Save Workbook
   wb.Close(SaveChanges=True)
-  # Tutup Excel
+  
+  # Close Excel.Application
   excel.Quit()
     
 if __name__ == "__main__":
   main()
 
-  # Unknown Behaviour
-  # Berguna untuk mengkalkulasi ulang PivotTable. Refer To: https://docs.microsoft.com/en-us/office/vba/api/excel.application.calculatefull
+  # Need Research
+  # Refer To: https://docs.microsoft.com/en-us/office/vba/api/excel.application.calculatefull
   #excel.CalculateFull()
